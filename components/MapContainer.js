94f2022ddf37d3b3b998b0936cad1160f06e425f
nicfb <bowennicf@gmail.com>
2022-02-11 09:18:01 -0600

    feat: search sites by name or number

--- components/MapContainer.js
+++ components/MapContainer.js
@@ -1,21 +1,22 @@
-import React, {useState, useEffect, useCallback} from "react";
+import React, {useState, useEffect, useCallback, useMemo} from "react";
 import MapGL, {Marker, Popup} from "react-map-gl";
 import CustomMarker from "./CustomMarker";
 import CustomPopup from "./CustomPopup";
 
-export default function MapContainer({lat, lng, markers, addMarker}) {
+export default function MapContainer({latLng, markers, addMarker}) {
   const [selectedSite, setSelectedSite] = useState('');
   const [viewport, setViewport] = useState({});
   const [newMarker, setNewMarker]  = useState(null);
   const [newMarkerName, setNewMarkerName] = useState('');
+  const [keyword, setKeyword] = useState('');
 
   useEffect(() => {
     setViewport({
-      latitude: lat,
-      longitude: lng,
+      latitude: latLng[0],
+      longitude: latLng[1],
       zoom: 8
     })
-  }, [lat, lng]);
+  }, [latLng]);
 
   const closePopup = () => {
     setSelectedSite('');
@@ -48,77 +49,84 @@ export default function MapContainer({lat, lng, markers, addMarker}) {
 
   const MAPBOX_API_KEY = process.env.NEXT_PUBLIC_MAPBOX_API_KEY;
   
-  const memoizedMarkers = React.useMemo(() => markers.map(
-    (site, i) => (
+  const memoizedMarkers = useMemo(() => markers.filter(site => (
+      site.name.includes(keyword) || site.num.includes(keyword))
+    ).map((s, i) => (
       <CustomMarker
-              key={`site-${i}`}
-              idx={i}
-              site={site}
-              openPopup={openPopup} />
-    )
-  ), [markers]);
+        key={`site-${i}`}
+        idx={i}
+        site={s}
+        openPopup={openPopup} />
+  )), [markers, keyword]);
+
+  const handleSearchChange = (e) => {
+    setKeyword(e.target.value.toUpperCase());
+  }
   
   return (
-    <MapGL
-      {...viewport}
-      width="100vw"
-      height="90vh"
-      mapStyle="mapbox://styles/mapbox/dark-v9"
-      onViewportChange={setViewport}
-      mapboxApiAccessToken={MAPBOX_API_KEY}
-      onClick={clearAllMarkers}
-      onDblClick={showAddMarkerPopup}
-      doubleClickZoom={false}
-    >
-      {
-        memoizedMarkers
-      }
-      {
-        selectedSite !== '' &&
-        <CustomPopup
-          idx={selectedSite}
-          lat={markers[selectedSite].lat}
-          lng={markers[selectedSite].lng}
-          site={markers[selectedSite]}
-          closePopup={closePopup} />
-      }
-      {
-         newMarker ? (
-           <>
-            <Marker
-              latitude={newMarker.lat}
-              longitude={newMarker.lng}>
-              <svg viewBox="-8 0 55 65" height="20">
-                <path d="m 10 35 l 15 30 l 15 -30 A 20 20 180 1 0 10 35 z" fill="blue" />
-              </svg>
-            </Marker>
-            <Popup
-              latitude={newMarker.lat}
-              longitude={newMarker.lng}
-              onClose={closeNewMarkerPopup}
-              closeButton={true}
-              closeOnClick={false}
-            >
-              <div className="p-3">
-                <h1>Add a New Marker</h1>
-                <input type="text"
-                  className="form-control border-2 border-black rounded"
-                  name="Title"
-                  value={newMarkerName}
-                  onChange={e => setNewMarkerName(e.target.value)}
-                />
-                <button
-                  className="bg-blue-500 hover:bg-blue-700 rounded-lg shadow-md text-white font-semibold m-2 p-2"
-                  onClick={handleAddNewMarker(newMarkerName, newMarker.lat, newMarker.lng)}
-                >
-                  Save
-                </button>
-              </div>
-            </Popup>
-          </>
-        )
-        : null
-      }
-    </MapGL>
+    <>
+      <input value={keyword} onChange={handleSearchChange} placeholder='Search Markers' />
+      <MapGL
+        {...viewport}
+        width="100vw"
+        height="90vh"
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        onViewportChange={setViewport}
+        mapboxApiAccessToken={MAPBOX_API_KEY}
+        onClick={clearAllMarkers}
+        onDblClick={showAddMarkerPopup}
+        doubleClickZoom={false}
+      >
+        {
+          memoizedMarkers
+        }
+        {
+          selectedSite !== '' &&
+          <CustomPopup
+            idx={selectedSite}
+            lat={markers[selectedSite].lat}
+            lng={markers[selectedSite].lng}
+            site={markers[selectedSite]}
+            closePopup={closePopup} />
+        }
+        {
+          newMarker ? (
+            <>
+              <Marker
+                latitude={newMarker.lat}
+                longitude={newMarker.lng}>
+                <svg viewBox="-8 0 55 65" height="20">
+                  <path d="m 10 35 l 15 30 l 15 -30 A 20 20 180 1 0 10 35 z" fill="blue" />
+                </svg>
+              </Marker>
+              <Popup
+                latitude={newMarker.lat}
+                longitude={newMarker.lng}
+                onClose={closeNewMarkerPopup}
+                closeButton={true}
+                closeOnClick={false}
+              >
+                <div className="p-3">
+                  <h1>Add a New Marker</h1>
+                  <input type="text"
+                    className="form-control border-2 border-black rounded"
+                    name="Title"
+                    value={newMarkerName}
+                    onChange={e => setNewMarkerName(e.target.value)}
+                  />
+                  <button
+                    className="bg-blue-500 hover:bg-blue-700 rounded-lg shadow-md text-white font-semibold m-2 p-2"
+                    onClick={handleAddNewMarker(newMarkerName, newMarker.lat, newMarker.lng)}
+                  >
+                    Save
+                  </button>
+                </div>
+              </Popup>
+            </>
+          )
+          : null
+        }
+      </MapGL>
+    </>
   );
 }
